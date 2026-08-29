33396e83203c4da69b1b182f38d3a34cb670d503
Cesar Leonardo <leonardorojascesar@gmail.com>
2026-08-29 10:51:45 -0500

    commit inicial

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Estudiante estudiante1 = new Estudiante();
+        estudiante1.codigo = "E001";
+        estudiante1.nombre = "Carlos Gómez";
+        estudiante1.promedio = 16.5;
+        estudiante1.edad = 20;
+        estudiante1.becado = true;
+        estudiante1.sexo = 'M';
+
+        estudiante1.mostrarDatos();
+    }
+}
---
+++ src/Estudiante.java
@@ -0,0 +1,27 @@
+public class Estudiante {
+    // Atributos
+    String codigo;
+    String nombre;
+    double promedio;
+    int edad;
+    boolean becado;
+    char sexo;
+    
+    //Comportamiento - métodos
+    public void mostrarDatos() {
+        System.out.println("Los datos del estudiante son: ");
+        System.out.println("Codigo: " + codigo);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Edad: " + edad);
+        System.out.println("Es becado: " + becado);
+        System.out.println("Sexo: " + sexo);
+        System.out.println( " ");
+        //Netbeans y Intellij
+        //Sout
+        //Eclipse y VsCode
+        //sysout - syso
+
+    }
+
+}
