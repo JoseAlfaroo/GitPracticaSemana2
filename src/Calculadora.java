01573fe499bd01524a5b660910ebb744727746e8
Giancarlo Contreras Montes <giancmontes@gmail.com>
2026-08-29 11:55:59 -0500

    Agrega metodo multiplicar a la calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,4 +3,9 @@ public class Calculadora {
         double resultado = num1 + num2;
         System.out.println("RESULTADO SUMA: " + resultado);
     }
+
+    public void multiplicar(double num1, double num2) {
+        double resultado = num1 * num2;
+        System.out.println("RESULTADO MULTIPLICACION: " + resultado);
+    }
 }
